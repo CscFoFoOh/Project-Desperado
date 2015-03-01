961a8dde57ebbe2f3fa0e71ee5d4f1e0fd4ecd22
Alex Sears <alexwsears@gmail.com>
2015-03-01 17:10:40 -0500

    Configured default from address

--- api/config/application.rb
+++ api/config/application.rb
@@ -30,5 +30,9 @@ module Api
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.action_mailer.default_options = {
+      :from => "do-not-reply@pd.alexsears.com"
+    }
   end
 end
