318fd80c9a6ed5a0fbd04cd4008d8dd580a12818
Alex Sears <alexwsears@gmail.com>
2015-03-01 16:18:22 -0500

    Fixed issue in production config

--- api/config/environments/production.rb
+++ api/config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Set default options for host
-  config.action_mailer.default_url_options[:host] = 'pd.alexsears.com'
+  config.action_mailer.default_url_options = { host: 'pd.alexsears.com' }
 
   # Setup Mandrill
   config.action_mailer.smtp_settings = {
