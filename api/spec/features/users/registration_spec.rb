92f3ffeb04509a0a9450d7da0eece747cd2338d2
Tyler Blatt <vtylerblatt@gmail.com>
2015-03-02 21:30:45 -0500

    More changes to testing

---
+++ api/spec/features/users/registration_spec.rb
@@ -0,0 +1,34 @@
+# spec/features/users/registration_spec.rb
+
+require 'spec_helper'
+
+feature 'Registration', js: true do
+  let(:email) { Faker::Internet.email }
+
+  before do
+    @registration_page = RegistrationPage.new
+    @registration_page.visit
+  end
+
+  feature 'with valid inputs' do
+    let(:password) { Faker::Internet.password }
+    before { @registration_page.complete_form(email: email, password: password) }
+
+    scenario 'account creation' do
+      find('a', text: 'Sign out').click
+      login_page = LoginPage.new
+      login_page.visit
+      login_page.sign_in(email, password)
+      expect(page).to have_content('Sign out')
+    end
+
+    scenario 'sign-in upon account creation' do
+      expect(page).to have_content('Sign out')
+    end
+  end
+
+  scenario 'with a too-short password' do
+    @registration_page.complete_form(email: email, password: 'a')
+    expect(page).to have_content('Password is too short')
+  end
+end
