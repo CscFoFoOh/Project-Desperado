8aa56afff1e93e6eb4753d3456bb8f033d294fa6
Tyler Blatt <vtylerblatt@gmail.com>
2015-03-24 20:55:45 -0400

    Fixed the dumbest linespacing error ever

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   scope '/api' do
     mount_devise_token_auth_for 'User', at: '/auth'
     resources :memberships, except: [:new, :edit]    
-	resources :comments, except: [:new, :edit]
+	  resources :comments, except: [:new, :edit]
     resources :projects, except: [:new, :edit]
   end
 
