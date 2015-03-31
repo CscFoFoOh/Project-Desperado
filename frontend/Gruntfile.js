729ff4ba1bb52b5a1e3b4daa000bc245aa96a161
Alex Sears <alexwsears@gmail.com>
2015-03-30 23:42:10 -0400

    Fixed globbing on Grunt build to get all views

--- frontend/Gruntfile.js
+++ frontend/Gruntfile.js
@@ -325,7 +325,7 @@ module.exports = function (grunt) {
             '*.{ico,png,txt}',
             '.htaccess',
             '*.html',
-            'views/{,*/}*.html',
+            'views/**/*.html',
             'images/{,*/}*.{webp}',
             'fonts/{,*/}*.*'
           ]
