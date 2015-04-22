f4e524a418dc3cd2d4e65ee617d302c35f4fedab
Tyler Blatt <vtylerblatt@gmail.com>
2015-04-22 18:39:42 -0400

    Fixed project and user Factories

--- api/spec/factories/project.rb
+++ api/spec/factories/project.rb
@@ -2,8 +2,7 @@ FactoryGirl.define do
 	factory :project do 
 		name { Faker::App.name }
  		description { Faker::Lorem.paragraph }
- 		user
-		closed_at nil
-		ended_at nil 
+ 		closed_at Time.now
+		ended_at Time.now
 	end
 end
--- api/spec/factories/user.rb
+++ api/spec/factories/user.rb
@@ -5,7 +5,8 @@ FactoryGirl.define do
 		password { Faker::Internet.password(8) } 
 		password_confirmation { password } 
     uid {Faker::Number.digit}
-		
+		first_name {Faker::Name.name}
+		last_name {Faker::Name.name}
 		factory :confirmed_user do 
 			confirmed_at Time.zone.now 
 		end
