d0548ce2484a21a8f9f136b4418168f447c3369a
Tyler Blatt <vtylerblatt@gmail.com>
2015-04-27 19:21:54 -0400

    Added validation to the project model

--- api/app/models/project.rb
+++ api/app/models/project.rb
@@ -3,6 +3,10 @@ class Project < ActiveRecord::Base
   has_many :memberships, dependent: :destroy
   has_many :users, through: :memberships
 
+  validates_presence_of :name
+  validates_presence_of :description
+
+
   def invitations
     memberships.invitations
   end
